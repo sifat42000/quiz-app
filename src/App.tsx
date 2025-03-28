e1fa2eab596227e89fd9a53cd34f23ed02389a83
sifat42000 <rashiquzzamansifat@gmail.com>
2025-03-28 12:18:33 +0600

    quizControll is finish

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,13 @@ function App() {
 
   return (
    <div className='mt-32 '>
-    <h2 className='flex justify-center mb-9 text-3xl '>বাংলাদেশের কিছু সাধারণ প্রশ্ন</h2>
+    {
+      quizComplete === true? 
+      <h2 className='flex justify-center mb-9 text-3xl '>Quiz শেষ! এবার ফলাফল দেখো</h2> :
+      <h2 className='flex justify-center mb-9 text-3xl '>বাংলাদেশের কিছু সাধারণ প্রশ্ন</h2>
+    }
+   
+   
   {
     quizComplete === true?  <QuizSummary/>:<Question/>
     
